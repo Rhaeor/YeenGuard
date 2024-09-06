c01ecf37a90ffc48cb13b66dd6d0a60061ff2aa0
Rhaeor Koishae <Rhaeor@outlook.com>
2024-09-06 13:49:30 -0700

    Initial Launch

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login('YOUR_BOT_TOKEN');
